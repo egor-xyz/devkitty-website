a3d36f9458b092f2b2004ebef744f664a2b87c9f
Egor <i@egor.xyz>
2020-08-02 11:29:57 +0300

    New header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,17 +2,26 @@ import React, { FC } from 'react';
 import { ReactComponent as Logo } from 'assets/svg/logo.svg';
 
 import css from './Header.module.scss';
+import { AnchorButton } from '@blueprintjs/core';
 
-export const Header:FC = () => (
+export const Header: FC = () => (
   <div className={css.root}>
-    <div className={css.wrap} >
-      <Logo className={css.logo} />
+    <div className={css.wrap}>
+      <Logo className={css.logo}/>
       <div className={css.logoText}>Git Branch Manager</div>
-      <nav className={css.menu} >
+      <nav className={css.menu}>
         <ul>
           <li><a href='/#'>Home</a></li>
           <li><a href='#features'>Features</a></li>
-          <li><a href='#downloads'>Downloads</a></li>
+          <li>
+            <AnchorButton
+              icon={'download'}
+              intent={'primary'}
+              className={css.blue}
+              href='#downloads'
+              text={'Download'}
+            />
+          </li>
         </ul>
       </nav>
     </div>
