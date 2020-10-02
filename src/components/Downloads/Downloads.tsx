eee52725a4785a71af79ef3711a3b8a35d8cdca1
Egor Stronhin <i@egor.xyz>
2020-10-02 12:12:20 +0300

    Easy app version change

--- src/components/Downloads/Downloads.tsx
+++ src/components/Downloads/Downloads.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ReactComponent as Apple } from 'assets/svg/apple.svg';
 import { ReactComponent as Windows } from 'assets/svg/windows.svg';
 
@@ -12,50 +12,48 @@ interface State {
   winURL?: string,
 }
 
-const version = '1.1.1';
+const version = process.env.REACT_APP_VERSION;
+const macURL = `https://github.com/egor-xyz/devkitty-app/releases/download/v${version}/Devkitty.setup.${version}.dmg`;
+const winURL = `https://github.com/egor-xyz/devkitty-app/releases/download/v${version}/Devkitty-Setup-${version}.exe`;
 
-export class Downloads extends Component<{}, State> {
-  render() {
-    const macURL = `https://github.com/egor-xyz/devkitty-app/releases/download/v${version}/Devkitty.setup.${version}.dmg`;
-    const winURL = `https://github.com/egor-xyz/devkitty-app/releases/download/v${version}/Devkitty-Setup-${version}.exe`;
-    return (
-      <div className={css.root}>
-        <div className={css.wrap}>
-          <div className={css.title}>devkitty</div>
-          <div className={css.version}>
-            <Icon icon={'confirm'}/>
-            <span>{version ?? 'Loading...'}</span>
-          </div>
-          <div className={css.actions}>
-            <div className={css.system}>
-              <Apple
-                height={50}
-                fill={'#fff'}
-              />
-              <AnchorButton
-                href={macURL}
-                className={css.btn}
-                icon={'download'}
-                large={true}
-                text={'devkitty for MacOS'}
-              >
-              </AnchorButton>
-            </div>
-            <div className={css.system}>
-              <Windows
-                height={50}
-                fill={'#fff'}
-              />
-              <AnchorButton
-                href={winURL}
-                className={css.btn}
-                large={true}
-                icon={'download'}
-                text={'devkitty for Windows 10'}
-              />
-            </div>
-          </div>
+export const Downloads = () => {
+  if (!version) return null;
+  return (<div className={css.root}>
+    <div className={css.wrap}>
+      <div className={css.title}>devkitty</div>
+      <div className={css.version}>
+        <Icon icon={'confirm'}/>
+        <span>{version ?? 'Loading...'}</span>
+      </div>
+      <div className={css.actions}>
+        <div className={css.system}>
+          <Apple
+            height={50}
+            fill={'#fff'}
+          />
+          <AnchorButton
+            href={macURL}
+            className={css.btn}
+            icon={'download'}
+            large={true}
+            text={'devkitty for MacOS'}
+          >
+          </AnchorButton>
         </div>
-      </div>);
-  }
+        <div className={css.system}>
+          <Windows
+            height={50}
+            fill={'#fff'}
+          />
+          <AnchorButton
+            href={winURL}
+            className={css.btn}
+            large={true}
+            icon={'download'}
+            text={'devkitty for Windows 10'}
+          />
+        </div>
+      </div>
+    </div>
+  </div>);
 }
