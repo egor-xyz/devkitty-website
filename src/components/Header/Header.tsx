97fc0ce43dbf9df59e213758462c1c5713939987
Egor Stronhin <i@egor.xyz>
2020-11-29 10:06:45 +0200

    1.1.9 Only macOS

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,43 @@
 import React, { FC } from 'react';
+import { motion } from 'framer-motion';
+
+import { DevKittyLogo } from 'assets/svg';
 
 import css from './Header.module.scss';
-import { DevKittyLogo } from 'assets/svg';
+
+const MotionSpan = motion.span;
 
 export const Header: FC = () => (
   <div className={css.root}>
     <div className={css.wrap}>
       <DevKittyLogo className={css.logo}/>
 
-      <div className={css.logoText}>devkitty.app</div>
+      <div className={css.logoText}>
+        devkit
+        <MotionSpan
+          transition={{
+            loop: Infinity,
+            duration: 2,
+            ease: 'linear',
+          }}
+          initial={{
+            opacity: .5,
+            y: '10px',
+          }}
+          animate={{
+            opacity: [.5, 0, .5],
+          }}
+        >ty</MotionSpan>
+      </div>
       <nav className={css.menu}>
         <ul>
           <li><a href='/#'>Home</a></li>
           <li><a href='#features'>Features</a></li>
-          <li><a href='#downloads'>Download</a></li>
+          <li>
+            <a href='#downloads'
+               className={css.blue}
+            >Download</a>
+          </li>
         </ul>
       </nav>
     </div>
