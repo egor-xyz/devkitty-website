98ed4bd8499b9daf6b6b1f3379dc897ada8c4259
egor-xyz <i@egor.xyz>
2023-07-09 11:54:39 +0300

    feat: v.2.0.0

--- src/components/Downloads/Downloads.tsx
+++ src/components/Downloads/Downloads.tsx
@@ -7,7 +7,8 @@ import { version } from 'version';
 import css from './Downloads.module.scss';
 
 export const Downloads = () => {
-  const macURL = `https://github.com/egor-xyz/devkitty/releases/download/${version}/Devkitty-mac-arm64-x64-${version.replace('v', '')}.zip`;
+  const macURLIntel = `https://github.com/egor-xyz/devkitty/releases/download/${version}/Devkitty-darwin-arm64-${version.replace('v', '')}.zip`;
+  const macURLApple = `https://github.com/egor-xyz/devkitty/releases/download/${version}/Devkitty-darwin-x64-${version.replace('v', '')}.zip`;
 
   return (<div className={css.root}>
     <div className={css.wrap}>
@@ -26,10 +27,18 @@ export const Downloads = () => {
         <div className={css.system}>
           <AnchorButton
             className={css.btn}
-            href={macURL}
+            href={macURLIntel}
             icon={'download'}
             large={true}
-            text={'Download (macOS)'}
+            text={'Download (Intel)'}
+          />
+
+          <AnchorButton
+            className={css.btn}
+            href={macURLApple}
+            icon={'download'}
+            large={true}
+            text={'Download (Apple Silicon)'}
           />
         </div>
       </div>
