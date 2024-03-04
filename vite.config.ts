fa653401c9369dd9f291adfdf6c8cf96c18414ca
egor-xyz <i@egor.xyz>
2024-03-04 20:58:00 +0200

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ import { checker } from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'build',
-  },
   plugins: [
     react(), 
     viteTsconfigPaths(),
