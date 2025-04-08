2ed3f089560fffbbcf0b87e8d3a52c7bffa23d45
Egor Stronhin <egorst@accessibe.com>
2025-04-08 22:39:47 +0300

    feat: add tailwind

--- src/components/Downloads/Downloads.tsx
+++ src/components/Downloads/Downloads.tsx
@@ -2,43 +2,44 @@ import { AnchorButton, Icon } from '@blueprintjs/core';
 import DevkittyLogo from 'assets/svg/devKitty.svg?react';
 import { useVersion } from 'store/useVersions';
 
-import css from './Downloads.module.scss';
-
 export const Downloads = () => {
   const { download, version } = useVersion();
 
   return (
-    <div className={css.root}>
-      <div className={css.wrap}>
+    <div
+      className='flex justify-center py-16 sticky top-0 bg-gradient-to-b from-blue-500 via-purple-500 to-orange-500'
+      style={{ height: 'calc(100vh - var(--header-height))' }}
+    >
+      <div className='flex flex-col items-center w-full '>
         <img
           alt='devkitty'
-          className={css.image}
+          className='max-w-full w-[800px] rounded-lg h-auto'
           src='/images/GitHub-App-Demo.png'
         />
 
-        <DevkittyLogo className={css.logo} />
-        <div className={css.title}>devkitty</div>
+        <DevkittyLogo className='w-16 h-16 mt-4' />
+        <div className='text-3xl font-bold text-center mt-2 text-white'>devkitty</div>
 
-        <div className={css.version}>
+        <div className='flex items-center gap-2 text-green-200 font-medium'>
           <Icon icon={'confirm'} />
           <span>{version}</span>
         </div>
 
-        <div className={css.actions}>
-          <div className={css.system}>
+        <div className='mt-4'>
+          <div className='flex flex-col gap-2'>
             <AnchorButton
-              className={css.btn}
+              className='w-full flex items-center justify-center gap-2 border border-gray-300 text-gray-700 font-medium py-2 rounded hover:bg-gray-100'
               href={download.apple()}
               icon={'download'}
-              large
+              size='large'
               text={'Download (Apple)'}
             />
 
             <AnchorButton
-              className={css.btn}
+              className='w-full flex items-center justify-center gap-2 border border-gray-300 text-gray-700 font-medium py-2 rounded hover:bg-gray-100'
               href={download.intel()}
               icon={'download'}
-              large
+              size='large'
               text={'Download (Intel)'}
             />
           </div>
